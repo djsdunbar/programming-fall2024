267c34bd596c69683e827a408b467fdb631707f5
Spencer Dunbar <spencerdunbar@Spencers-MacBook-Air-3.local>
2024-11-21 10:53:11 -0600

    Javascript-madlib

---
+++ learning-javascript/Javascript-madlib/Script.js
@@ -0,0 +1,37 @@
+function generate() {
+    const a1 = document.getElementById("a1input").value;
+    const n1 = document.getElementById("n1input").value;
+
+    document.getElementById("a1").innerText = a1;
+    document.getElementById("n1").innerText = n1;
+    
+    const v1 = document.getElementById("v1input").value;
+    const adverb1 = document.getElementById("adverb1input").value;
+
+    document.getElementById("v1").innerText = v1;
+    document.getElementById("adverb1").innerText = adverb1;
+    
+    const name1 = document.getElementById("name1input").value;
+    const place1 = document.getElementById("place1input").value;
+
+    document.getElementById("name1").innerText = name1;
+    document.getElementById("place1").innerText = place1;
+    
+    const s1 = document.getElementById("s1input").value;
+    const v2 = document.getElementById("v2input").value;
+
+    document.getElementById("s1").innerText = s1;
+    document.getElementById("v2").innerText = v2;
+
+    const e1 = document.getElementById("e1input").value;
+    const n2 = document.getElementById("n2input").value;
+
+    document.getElementById("e1").innerText = e1;
+    document.getElementById("n2").innerText = n2;
+    
+    const a2 = document.getElementById("a2input").value;
+    const p2 = document.getElementById("p2input").value;
+
+    document.getElementById("a2").innerText = a1;
+    document.getElementById("p2").innerText = p2;
+}
