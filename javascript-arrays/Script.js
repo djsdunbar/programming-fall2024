12b51ec0aa3283ca6df778395c4d93775f78e548
Spencer Dunbar <spencerdunbar@Spencers-MacBook-Air-3.local>
2024-12-04 15:35:25 -0700

    javascript-arrays

---
+++ javascript-arrays/Script.js
@@ -0,0 +1,43 @@
+//let myList = [ "go home", "take a shower", "do homework"];
+//// grab the first item
+//alert(myList[0]);
+//
+//// change the first item
+//myList[0]="make dinner";
+//alert(myList);
+//// adds an item to the end
+//myList.push("do chores");
+//alert(myList);
+//// removes the item at the end
+//myList.pop("cry");
+//alert(myList);
+//
+//myList.splice(2,3);
+//alert(myList);
+
+//let groceries = ["milk", "fruit", "bread", "eggs"];
+//groceries.splice(1,1, "ice cream");
+//alert(groceries);
+//
+//groceries.splice(0,1)
+//alert(groceries)
+
+//Activity 1
+//let task = ["wake up", "wash my face", "put on lotion", "get dressed"];
+//console.log(task);
+//task.push("eat breakfast", "go to school");
+//console.log("eat breakfast");
+//
+//task.pop(task);
+//console.log(task);
+//
+//task.splice(0,1, "cry");
+//console.log(task);
+//
+//task.splice(0,2);
+
+//Activity 2
+
+let numbers =["5","6","7"];
+
+
